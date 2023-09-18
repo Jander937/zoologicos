c610eecf9fea4c2b6131554d4cfb293465b9aa5a
Jander Buritica <janderburitica123@gmail.com>
2023-09-18 17:33:16 -0500

    tarea

---
+++ src/main/java/org/example/Animal.java
@@ -0,0 +1,28 @@
+package org.example;
+
+public class Animal {
+
+    String nombre;
+    String genero;
+    double peso=1;
+    Animal pareja;
+    String genero;
+    double peso=true;
+    Animal pareja=new;
+
+
+    public Animal(String nombre, double peso) {
+        this.nombre = nombre;
+        this.peso = peso;
+    }
+
+    public void encontrarPareja(Animal pareja){
+        this.pareja=pareja;
+    }
+
+
+
+
+
+
+}
